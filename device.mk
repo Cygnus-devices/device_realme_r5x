1932bb7cd5cafb998ca16a1e92ed3b57807b91e0
adi8900 <adrianszymanski242@gmail.com>
2021-02-16 13:16:21 +0100

    r5x: Remove unwanted google packages
    Signed-off-by: MadhavSaladi <baaswanthmadhav@gmail.com>

--- device.mk
+++ device.mk
@@ -394,6 +394,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
+# Remove Unwanted Packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.4 \
